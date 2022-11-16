1e076134346385119c894bd2af63e5859d1c0d0f
AllanG-4779 <allang4779@gmail.com>
2022-11-16 16:12:37 +0300

    cors error

--- src/main/java/org/jiranibora/com/twilio/SMSController.java
+++ src/main/java/org/jiranibora/com/twilio/SMSController.java
@@ -6,16 +6,14 @@ import org.jiranibora.com.payment.FineRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 
 @RestController
 @AllArgsConstructor
+@CrossOrigin(origins = {"*"})
 public class SMSController {
     private final SMSending smSendingService;
     private final FineRepository fineRepository;
